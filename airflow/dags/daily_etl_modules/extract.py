141cc0677b5f3c387430f1821e915cd2a910a1e9
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2023-08-25 16:00:48 -0600

    Aws module working

--- airflow/dags/daily_etl_modules/extract.py
+++ airflow/dags/daily_etl_modules/extract.py
@@ -1,10 +1,12 @@
 import requests
 import gzip
-import datetime
+import boto3
 import shutil
-from utils import logger, logger_verbose
 import os
 from dotenv import load_dotenv
+from typing import Any, Optional
+import logging
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -15,64 +17,44 @@ def extract_raw_file(url: str = API_URL) -> gzip.GzipFile:
     ''' Requests the endpoint and retrieve the file compressed
 
     Args:
-        url (str): url of the endpoint. Defaults to "https://smn.conagua.gob.mx/webservices/?method=3"
+        url (str): url of the endpoint. Defaults to "https://smn.conagua.gob.mx/tools/GUI/webservices/?method=1"
 
     Returns:
         gzip.GzipFile: Route of the compressed file
     '''
     try:
-        # today =  datetime.datetime.today().isoformat()
+        logger.info(msg='Requesting endpoint')
         ftpstream = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, timeout=10)
-        # with open(f'/opt/airflow/data/raw/HourlyForecast_MX_{today}.gz', 'wb') as file:
-        #     file.write(ftpstream.content)
-        #     logger.info(msg='Extract raw file successful')
-        #     return f'/opt/airflow/data/raw/HourlyForecast_MX_{today}.gz'
     except Exception as e:
         logger.exception(e)
         logger.error(msg='Extract raw file failed')
         raise ValueError
-        
+    logger.info(msg='Request successful')
     return ftpstream.content
 
-def extract_json(filepath:str) -> None:
-    '''Uncompress the data in gzip format and returns a json format
+if __name__ == '__main__':
+    import logging
+    from jsonformatter import JsonFormatter
 
-    Args:
-        filepath (str): Path of the gzip file
 
-    Returns:
-        json: Json data format file
-    '''
-    #today = datetime.date.today().isoformat()
-    try:
-        with gzip.open(filepath, 'rb') as f_in:
-            with open(f'/opt/airflow/data/intermediate/HourlyForecast_MX.json', 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        logger.info(msg='Extract json file successful')
-        return 
-    except Exception as e:
-        logger.exception(e)
-        logger.error(msg='Extract json file failed')
-        raise ValueError
-        
-@logger_verbose
-def extract(url: str = "https://smn.conagua.gob.mx/webservices/?method=3") -> None:
-    '''Wrapper that extracts data from endpoint and converts the gzip into json format 
 
-    Args:
-        url (_type_, optional): Endpoint url. Defaults to "https://smn.conagua.gob.mx/webservices/?method=3".
+    STRING_FORMAT = '''{
+        "Name":            "name",
+        "Levelname":       "levelname",
+        "FuncName":        "funcName",
+        "Timestamp":       "asctime",
+        "Message":         "message"
+    }'''
 
-    Returns:
-        json: Data in Json format
-    '''
-    try:
-        logger.info('Starting extract process')
-        path = extract_raw_file(url)
-        extract_json(path)
-    except:
-        logger.error('Extract process failed')
-        raise ValueError
-        #logger.error(e)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = JsonFormatter(STRING_FORMAT)
 
-# if __name__ == '__main__':
-#     extract('asas')
+    logHandler = logging.StreamHandler()
+    logHandler.setFormatter(formatter)
+    logHandler.setLevel(logging.INFO)
+
+    logger.addHandler(logHandler)
+
+    file = extract_raw_file()
+    print(type(file))
