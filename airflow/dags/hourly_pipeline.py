5b2be09cde5c7949afe5cd806233b6290dd9819f
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2023-09-17 21:17:52 -0600

    Hourly pipeline works. Docker file removed copy statements. Added queries to toml.

--- airflow/dags/hourly_pipeline.py
+++ airflow/dags/hourly_pipeline.py
@@ -36,21 +36,22 @@ bucket = os.environ['S3_BUCKET']
 
 bucket = os.environ['S3_BUCKET']
 queries = toml.load('queries.toml')
-base_table_hourly_query = queries['base_table_hourly']['creation_query']
-query_hourly_aggregate = queries['aggregations_hourly']['example_query']
+base_table_hourly_query = queries['base_table']['hourly_query']
+query_hourly_aggregate = queries['aggregations']['example_query_hourly']
+test = queries['aggregations']['test']
 filename = 'HourlyForecast_MX.gz'
 
 
 with DAG(
-    'weather_dag',
+    'hourly_weather_dag',
     default_args=default_args,
     description='ETL for the weahter web service',
     schedule_interval='0 * * * *', catchup=False
 ) as dag:
     create_base_table = BigQueryExecuteQueryOperator(task_id='create_hourly_base_table', sql=base_table_hourly_query, use_legacy_sql=False, gcp_conn_id='GCP')
     upload_s3_task = S3CreateObjectOperator(s3_bucket=bucket,s3_key="{{ds}}/" + filename, task_id='upload_s3', data=_extract_raw_file(os.environ['CONAGUA_API_HOURLY']), aws_conn_id='AWS', replace=True)
-    load_raw_table = S3ToBigQuery(s3_client=s3, bq_client=bq, bucket=bucket, bq_table='raw_hourly_table', filename=filename, task_id='load_raw_hourly_table')
-    aggregate_examples = BigQueryExecuteQueryOperator(task_id='aggregate_houryl_examples', sql=query_hourly_aggregate, use_legacy_sql=False, gcp_conn_id='GCP',
+    load_raw_table = S3ToBigQuery(s3_client=s3, bq_client=bq, bucket=bucket, bq_table='hourly_raw_table', filename=filename, task_id='load_raw_hourly_table')
+    aggregate_examples = BigQueryExecuteQueryOperator(task_id='aggregate_hourly_examples', sql=query_hourly_aggregate, use_legacy_sql=False, gcp_conn_id='GCP',
                                                       destination_dataset_table=f"{bq_project}.{bq_dataset}.aggregated_hourly_examples", write_disposition='WRITE_TRUNCATE')
     
     create_base_table >> upload_s3_task >> load_raw_table >> aggregate_examples
